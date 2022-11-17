7e71744536a5369c5b622379cab1e8e59503ccee
Mickey <mickey@Mihits-MacBook-Pro.local>
2022-11-17 16:07:33 +0530

    sdk demo code

--- angular-demo/src/app/app.component.ts
+++ angular-demo/src/app/app.component.ts
@@ -1,117 +1,31 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import otplessSdk from 'otpless-test/dist';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  //Global Functions
-  getURLParameter = (paramName: string, URLString = window.location.href) => {
-    var regex = new RegExp('[\\?&]' + paramName + '=([^&#]*)');
-    var results = regex.exec(URLString);
-
-    if (results && results.length > 0) {
-      return decodeURIComponent(results[1].replace(/\+/g, ' '));
-    } else {
-      return '';
-    }
-  };
-
   //Constants
   appId = 'OTPLess:NPMVXHQQAZBBNOXLWIIDTSLPWTABFXQY';
-  headers = { 'Content-Type': 'application/json', appId: this.appId };
-  baseUrl = 'https://api.otpless.app/v1/client/user/session';
-  sessionVerified = false;
-
-  //Api Maps
-  initiateLoginMap = {
-    url: `${this.baseUrl}/initiate`,
-    data: {
-      loginMethod: 'WHATSAPP',
-      redirectionURL: 'http://localhost:4200',
-    },
-    response: {},
-  };
-
-  getAppSecretMap = {
-    url: `https://your.domain.com/getAppSecret`,
-    response: {},
-  };
-
-  getUserDetailsMap: any = {
-    url: `${this.baseUrl}/userdata`,
-    data: {
-      token: this.getURLParameter('token'),
-    },
-    response: {},
-  };
-
-  //Constructor
-  constructor(private http: HttpClient) {}
+  token = '';
+  sdkIntance: any;
+  getIntent: any;
 
   //Functions
-  getApiCall(apiDetails: any) {
-    return this.http.get(apiDetails.url, {
-      headers: {
-        ...this.headers,
-        ...(apiDetails.headers && { ...apiDetails.headers }),
-      },
-    });
-  }
-
-  postApiCall(apiDetails: any) {
-    return this.http.post(apiDetails.url, apiDetails.data, {
-      headers: {
-        ...this.headers,
-        ...(apiDetails.headers && { ...apiDetails.headers }),
-      },
-    });
-  }
-
   ngOnInit() {
-    if (this.getURLParameter('token')) {
-      localStorage.setItem('token', this.getURLParameter('token'));
-      this.getAppSecret();
-    }
-  }
-
-  resetSession() {
-    this.sessionVerified = false;
+    this.sdkIntance = otplessSdk({
+      appId: 'OTPLess:VHJMCSOXWSLTZBEMPOFRDAPPYSBFGXSE',
+      enableErrorLogging: true,
+    });
+    let UrlToken = this.sdkIntance.getToken();
+    this.token = UrlToken ?? '';
   }
 
   initiateLogin() {
-    const apiCall = this.postApiCall(this.initiateLoginMap);
-    apiCall.subscribe((data: any) => {
-      window.location.assign(data.data.intent);
+    this.getIntent = this.sdkIntance.getIntent({
+      redirectionURL: 'http://localhost:4200',
     });
-  }
-
-  getAppSecret() {
-    const apiCall = this.getApiCall(this.getAppSecretMap);
-    apiCall.subscribe((data: any) => {
-      this.getUserDetails(data.appSecret);
-    });
-  }
-
-  getUserDetails(appSecret: string) {
-    const apiCall = this.postApiCall({
-      ...this.getUserDetailsMap,
-      headers: {
-        appSecret,
-      },
-    });
-    apiCall.subscribe((data: any) => {
-      window.history.pushState({}, document.title, '/');
-      const details = data.data;
-      if (details.stateMatched) {
-        this.sessionVerified = true;
-        this.getUserDetailsMap.response = details;
-        console.log({
-          getUserDetailsMap: this.getUserDetailsMap,
-        });
-      }
-    });
+    return this.getIntent();
   }
 }
--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,146 +1,47 @@
 import React, { useEffect, useState } from "react";
 import WhatsappIcon from "./whatsapp.svg";
 import "./App.scss";
+import otplessSdk from "otpless-test/dist";
 
 const App = () => {
-  //Global Functions
-  const getURLParameter = (
-    paramName: string,
-    URLString = window.location.href
-  ) => {
-    var regex = new RegExp("[\\?&]" + paramName + "=([^&#]*)");
-    var results = regex.exec(URLString);
-
-    if (results && results.length > 0) {
-      return decodeURIComponent(results[1].replace(/\+/g, " "));
-    } else {
-      return "";
-    }
-  };
-
   //Constants
-  const appId = "OTPLess:VHJMCSOXWSLTZBEMPOFRDAPPYSBFGXSE";
-  const headers = { "Content-Type": "application/json", appId };
-  const baseUrl = "https://api.otpless.app/v1/client/user/session";
-  const [sessionVerified, setSessionVerified] = useState(false);
-
-  //Api Maps
-  const initiateLoginMap = {
-    url: `${baseUrl}/initiate`,
-    data: {
-      loginMethod: "WHATSAPP",
-      redirectionURL: "http://localhost:3001",
-    },
-    response: {},
-  };
-
-  const getAppSecretMap = {
-    url: `https://your.domain.com/getAppSecret`,
-    response: {},
-  };
-
-  const getUserDetailsMap: any = {
-    url: `${baseUrl}/userdata`,
-    data: {
-      token: getURLParameter("token"),
-    },
-    response: {},
-  };
+  let sdkIntance: any;
+  let getIntent: any;
+  const [token, setToken] = useState("");
 
   //useEffects
   useEffect(() => {
-    if (getURLParameter("token")) {
-      localStorage.setItem("token", getURLParameter("token"));
-      getAppSecret();
-    }
+    sdkIntance = otplessSdk({
+      appId: "OTPLess:VHJMCSOXWSLTZBEMPOFRDAPPYSBFGXSE",
+      enableErrorLogging: true,
+    });
+    let UrlToken = sdkIntance.getToken();
+    UrlToken && setToken(UrlToken);
   }, []);
 
   //Functions
-  const getApiCall = async (apiDetails: any) => {
-    return await fetch(apiDetails.url, {
-      method: "GET",
-      headers: {
-        ...headers,
-        ...(apiDetails.headers && { ...apiDetails.headers }),
-      },
-    }).then((response) => response.json());
-  };
-
-  const postApiCall = async (apiDetails: any) => {
-    return await fetch(apiDetails.url, {
-      method: "POST",
-      headers: {
-        ...headers,
-        ...(apiDetails.headers && { ...apiDetails.headers }),
-      },
-      body: JSON.stringify(apiDetails.data),
-    }).then((response) => response.json());
-  };
-
-  const resetSession = () => {
-    setSessionVerified(false);
-  };
-
-  const getAppSecret = () => {
-    const apiCall = getApiCall(getAppSecretMap);
-    apiCall.then((data) => {
-      getUserDetails(data.appSecret);
-    });
-  };
-
   const initiateLogin = () => {
-    const apiCall = postApiCall(initiateLoginMap);
-    apiCall.then((data: any) => {
-      window.location.assign(data.data.intent);
+    getIntent = sdkIntance.getIntent({
+      redirectionURL: "http://localhost:3001",
     });
-  };
-
-  const getUserDetails = (appSecret: string) => {
-    const apiCall = postApiCall({
-      ...getUserDetailsMap,
-      headers: {
-        appSecret,
-      },
-    });
-    apiCall.then((data: any) => {
-      window.history.pushState({}, document.title, "/");
-      const details = data.data;
-      if (details.stateMatched) {
-        setSessionVerified(true);
-        getUserDetailsMap.response = details;
-        console.log({
-          getUserDetailsMap,
-        });
-      }
-    });
+    return getIntent();
   };
 
   return (
     <main className="flex algn-cntr jstfy-cntr">
-      {sessionVerified ? (
-        <div className="response flex algn-cntr jstfy-cntr">
-          <p className="success">Your Session has been verified</p>
-          <div className="details">
-            <p>
-              Name: <span>{getUserDetailsMap.response.name ?? "Mickey"}</span>
-            </p>
-            <p>
-              Mobile:
-              <span>{getUserDetailsMap.response.mobile ?? "898099455"}</span>
-            </p>
-          </div>
-          <button onClick={() => resetSession()}>Reset</button>
+      <button
+        className="login-with-whatsapp-button"
+        onClick={() => initiateLogin()}
+      >
+        <div className="flex algn-cntr">
+          <img src={WhatsappIcon} />
+          <p>Login With Whatsapp</p>
         </div>
-      ) : (
-        <button
-          className="login-with-whatsapp-button"
-          onClick={() => initiateLogin()}
-        >
-          <div className="flex algn-cntr">
-            <img src={WhatsappIcon} />
-            <p>Login With Whatsapp</p>
-          </div>
-        </button>
+      </button>
+      {token && (
+        <p className="token">
+          Your Token: <span>{token}</span>
+        </p>
       )}
     </main>
   );
